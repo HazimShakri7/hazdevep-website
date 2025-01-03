676b05eaf0857452ffdd855216e9dcd49c9973b6
Haz Developer <hazimshakri7@gmail.com>
2025-01-03 16:47:08 +0800

    components: engage

---
+++ src/app/shared/Engage.tsx
@@ -0,0 +1,18 @@
+export default function Engage() {
+    return (
+        <div className="flex mt-10 grid-cols-1 sm:grid-cols-3 gap-12 text-center">
+            <div>
+                <p className="flex text-5xl font-bold">2+</p>
+                <p className="text-lg">Years Experience</p>
+            </div>
+            <div>
+                <p className="flex text-5xl font-bold">10+</p>
+                <p className="text-lg">Website Delivered</p>
+            </div>
+            <div>
+                <p className="flex text-5xl font-bold">20+</p>
+                <p className="text-lg">Happy Customers</p>
+            </div>
+        </div>
+    );
+}
