99cefd94add51bb05c1326708b1bbff8f6012e92
Haz Developer <hazimshakri7@gmail.com>
2025-01-21 13:02:09 +0800

    add layout.tsx

--- src/app/privacy-policy/layout.tsx
+++ src/app/privacy-policy/layout.tsx
@@ -1,24 +1,17 @@
-// src/app/layout.tsx
 import React from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const PrivacyPolicyLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <head>
-        <title>Layout for Privacy Policy</title>
-        {/* Add other mTeta tags or links to stylesheets here */}
-      </head>
-      <body>
-        <header>
-          {/* Your site header (navigation, logo, etc.) */}
-          <h1>PrivacyPolicy</h1>
-        </header>
-        <main>{children}</main>
-        <footer>
-          {/* Your site footer */}
-          <p>&copy; PrivacyPolicy</p>
-        </footer>
-      </body>
-    </html>
+    <div>
+      <header>
+        <h1>Privacy Policy</h1>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <p>&copy; 2025 My Application</p>
+      </footer>
+    </div>
   );
-}
+};
+
+export default PrivacyPolicyLayout;
